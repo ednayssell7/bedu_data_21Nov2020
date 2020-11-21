19354e42f030b470f67bf61b486bc6612d2e9e57
ednayssell7 <ednayrr@gmail.com>
2020-11-21 13:05:41 -0700

    Tabla multiplicar & tarea

--- 02_funcion_range.py
+++ 02_funcion_range.py
@@ -1,5 +1,5 @@
 # explian how range function works in Python
 
+#refresh on how f strings work
 for n in range(100):
     print(f'Imprimiendo el valor de n{n}')
-    
---
+++ 03_tabla_multiplicar.py
@@ -0,0 +1,18 @@
+#script que calcule la tabla de multiplicar de un numero
+#solicitar numero al usuario por consola
+numero = input('que numero quieres multiplicar?')
+#el dato ingresado es string
+#convertir a INT para poder multiplicar
+numero =int(numero)
+
+for n in range(10):
+    resultado = numero * (n + 1)
+    print(resultado)
+
+#TAREA
+# imprimir resultado asi
+#8 x 1 = 8
+#8 x 2 = 16
+#8 x 3 =32
+...
+

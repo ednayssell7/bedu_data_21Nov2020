8c66684690d202f36a4b0da92a2b07ba5465fd19
ednayssell7 <ednayrr@gmail.com>
2020-11-21 12:24:34 -0700

    added file to explain lists in python

---
+++ 01_listas.py
@@ -0,0 +1,18 @@
+#archivo explicando listas
+
+numeros = [23, 89, 127, 1289, 8912]
+print(numeros)
+
+#para agregar un elemento a una lista usamos append
+num_extra = 33
+num_extra2 =1223
+num_extra3 = 89
+
+numeros.append(num_extra)
+print(numeros)
+
+#para eliminiar el ultimo elemento de una lista usamos pop
+numeros.pop()
+print(numeros)
+
+
